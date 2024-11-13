79618749789d34672065896a4d4d36bb8315c94b
tonyfalcondevweb <tonyfalcondevweb@gmail.com>
2024-11-13 09:27:25 +0100

    fix api

--- api/products.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/products', (req, res) => {
-    res.json([{ id: 1, name: 'Produit A' }, { id: 2, name: 'Produit B' }]);
-});
-
-module.exports = app;
--- index.js
+++ index.js
@@ -9,5 +9,10 @@ app.get('/', (req, res) => {
     res.send('Bienvenue sur mon API Express déployée sur Vercel!');
 });
 
+app.get('/home', (req, res) => {
+    res.status(200).json('Welcome, your app is working well');
+  });
+  
+
 // Export de l'app pour Vercel
 module.exports = app;
